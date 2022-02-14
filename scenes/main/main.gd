1d4cb5cc56ae12909b397423cbafa0101ca71475
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-02-14 23:40:21 +0000

    first push

---
+++ scenes/main/main.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_pressed("restart"):
+		get_tree().change_scene("res://scenes/main/main.tscn")
---
+++ scenes/main/player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+export var speed : int = 8
+
+var health : int = 100
+
+var rng = RandomNumberGenerator.new()
+	
+func _init():
+	var HurtTimer = Timer.new()
+	add_child(HurtTimer)
+	HurtTimer.autostart = true
+	HurtTimer.wait_time = 1
+	HurtTimer.connect("timeout", self, "hurt_time")
+	
+func _ready():
+	rng.randomize()
+
+func hurt_time():
+	hurt(rng.randi_range(0, 30))
+
+func _physics_process(delta):
+	var move : Vector2 = lrud() * speed
+	var collision = move_and_collide(move)
+	if collision:
+		move_and_collide(collision.remainder.slide(collision.normal))
+	
+func lrud() -> Vector2:
+	var input : Vector2 = Vector2.ZERO
+	if Input.is_action_pressed('right'):
+		input.x += 1
+	if Input.is_action_pressed('left'):
+		input.x -= 1
+	if Input.is_action_pressed("up"):
+		input.y -= 1
+	if Input.is_action_pressed('down'):
+		input.y += 1
+	return input.normalized()
+
+func hurt(damage):
+	health -= damage
+	$HealthBar.value = health
+	if health <= 0:
+		get_tree().change_scene("res://scenes/empty/empty.tscn")
