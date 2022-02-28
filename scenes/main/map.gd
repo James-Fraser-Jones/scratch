c7c26800a92c5e088068a556fb391d943a1dd349
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-02-28 22:33:34 +0000

    cleaned up code a bit, ready to attempt optimized version

--- scenes/main/map.gd
+++ scenes/main/map.gd
@@ -12,7 +12,9 @@ export var cols: int = 10
 export var rows: int = 10
 export var threshold: float = 0
 
-export var gen: bool = false setget run_gen
+export var generate: bool setget run_generate
+export var delete: bool setget run_delete
+export var generate2: bool setget run_generate2
 
 var noise = OpenSimplexNoise.new()
 
@@ -23,13 +25,13 @@ const lookup = [
 	[Vector2(0,1),Vector2(2,1),Vector2(2,2),Vector2(0,2)],
 	
 	[Vector2(2,0),Vector2(2,1),Vector2(1,0)],
-	[Vector2(0,2),Vector2(0,1),Vector2(1,0),Vector2(2,0),Vector2(2,1),Vector2(1,2)],
+	[Vector2(0,2),Vector2(0,1),Vector2(1,0),Vector2(2,0),Vector2(2,1),Vector2(1,2)], #ambiguous
 	[Vector2(1,0),Vector2(2,0),Vector2(2,2),Vector2(1,2)],
 	[Vector2(1,0),Vector2(2,0),Vector2(2,2),Vector2(0,2),Vector2(0,1)],
 	
 	[Vector2(0,0),Vector2(1,0),Vector2(0,1)],
 	[Vector2(0,0),Vector2(1,0),Vector2(1,2),Vector2(0,2)],
-	[Vector2(0,0),Vector2(1,0),Vector2(2,1),Vector2(2,2),Vector2(1,2),Vector2(0,1)],
+	[Vector2(0,0),Vector2(1,0),Vector2(2,1),Vector2(2,2),Vector2(1,2),Vector2(0,1)], #ambiguous
 	[Vector2(0,0),Vector2(1,0),Vector2(2,1),Vector2(2,2),Vector2(0,2)],
 	
 	[Vector2(0,0),Vector2(2,0),Vector2(2,1),Vector2(0,1)],
@@ -38,24 +40,30 @@ const lookup = [
 	[Vector2(0,0),Vector2(2,0),Vector2(2,2),Vector2(0,2)],
 ]
 
-func run_gen(_b):
+func run_generate2(_b):
 	if Engine.is_editor_hint():
-		gen_()
+		pass
+		#try and do chunk by chunk instead
+
+func run_generate(_b):
+	if Engine.is_editor_hint():
+		update_noise_params()
+		remove_all_convex()
+		var noise_grid = get_noise_grid(rows+1, cols+1, noise)
+		var lookup_grid = get_lookup_grid(rows, cols, noise_grid, threshold)
+		add_from_lookup_grid(lookup_grid)
 		
-func gen_():
-	update_noise_params()
-	remove_all_convex()
-	var noise_grid = get_noise_grid(rows+1, cols+1, noise)
-	var lookup_grid = get_lookup_grid(rows, cols, noise_grid, threshold)
-	add_from_lookup_grid(lookup_grid)
+func run_delete(_b):
+	if Engine.is_editor_hint():
+		remove_all_convex()
 	
 func add_from_lookup_grid(lookup_grid):
-	for i in range(0, lookup_grid.size()):
-		for j in range(0, lookup_grid[0].size()):
-			var points = shallow_copy_array(lookup[lookup_grid[i][j]])
+	for j in range(0, lookup_grid.size()):
+		for i in range(0, lookup_grid[0].size()):
+			var points = shallow_copy_array(lookup[lookup_grid[j][i]])
 			var translate = Vector2(i, j) * 2
-			for k in range(0, points.size()):
-				points[k] += translate
+			for p in range(0, points.size()):
+				points[p] += translate
 			add_convex(points)
 
 func update_noise_params():
@@ -78,9 +86,9 @@ func get_lookup_val(tl, tr, bl, br, threshold) -> int:
 
 func get_lookup_grid(rows, cols, grid, threshold) -> Array:
 	var lookup_grid = []
-	for i in range(0, cols):
+	for j in range (0, rows):
 		var lookup_row = []
-		for j in range (0, rows):
+		for i in range(0, cols):
 			var tl = grid[i][j]
 			var tr = grid[i+1][j]
 			var bl = grid[i][j+1]
@@ -91,9 +99,9 @@ func get_lookup_grid(rows, cols, grid, threshold) -> Array:
 
 func get_noise_grid(rows, cols, noise) -> Array:
 	var noise_grid = []
-	for i in range (0, cols):
+	for j in range (0, rows):
 		var noise_row = []
-		for j in range (0, rows):
+		for i in range (0, cols):
 			noise_row.append(noise.get_noise_2d(i, j))
 		noise_grid.append(noise_row)
 	return noise_grid
@@ -116,3 +124,8 @@ func shallow_copy_array(arr):
 	for elem in arr:
 		new.append(elem)
 	return new
+
+#things missing:
+#dealing with ambiguous cases (currently just assuming middle is always filled) https://www.boristhebrave.com/2022/01/03/resolving-ambiguities-in-marching-squares/
+#using only a single collision shape per contiguous chunk from thresholded noise function (Geometry.convex_hull_2d() might be useful)
+#doing linear interpolation
