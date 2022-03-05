76fcb1995464dd38cf28b47d1cfd39b867cba6c6
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-03-05 16:03:16 +0000

    bug extremely slippery, we're going to give up on this for now

--- scenes/main/nav.gd
+++ scenes/main/nav.gd
@@ -11,6 +11,9 @@ export var bake: bool setget run_bake
 export var delete: bool setget run_delete
 export var test: bool setget run_test
 
+export var skip: Array = [5]
+export var flip: int = 0
+
 func run_test(_b):
 	if Engine.is_editor_hint():
 		remove_all_children()
@@ -32,71 +35,7 @@ func run_bake(_b):
 	if Engine.is_editor_hint() and map_path:
 		remove_all_children()
 		
-		var local_offset = offset
-		var outlines = []
-		
-		var bodies = get_node(map_path).get_children()
-		for body in bodies:
-			var colliders = body.get_children()
-			for collider in colliders:
-				
-				var poly
-				if collider is CollisionPolygon2D:
-					poly = collider.polygon
-				elif collider is CollisionShape2D:
-					var shape = collider.shape
-					if shape is RectangleShape2D:
-						var ext = shape.extents
-						poly = PoolVector2Array([Vector2(-ext.x, -ext.y), Vector2(ext.x, -ext.y), Vector2(ext.x, ext.y), Vector2(-ext.x, ext.y)])
-					elif shape is CircleShape2D:
-						var rad = shape.radius
-						var local_circle_res = max(3, int(circle_res * rad * circle_res_growth))
-						local_offset = (offset + rad)/cos(PI / local_circle_res) - rad #https://en.wikipedia.org/wiki/Apothem
-						poly = PoolVector2Array()
-						for i in range(0, local_circle_res):
-							poly.push_back((Vector2.UP*rad).rotated(i * (2 * PI)/local_circle_res))
-					else:
-						continue
-				else:
-					continue
-					
-				for i in range(0, poly.size()):
-					var temp = poly[i].rotated(collider.rotation) + collider.position
-					poly.set(i, temp.rotated(body.rotation) + body.position)
-					
-				var new_poly = Geometry.offset_polygon_2d(poly, local_offset, Geometry.JOIN_MITER)[0]
-				outlines.append(new_poly)
-		
-		if merge:
-			var i = 0
-			while i < outlines.size():
-				var j = 0
-				while j < outlines.size():
-					if j != i:
-						if Geometry.intersect_polygons_2d(outlines[i], outlines[j]).size() > 0:
-							var merge = Geometry.merge_polygons_2d(outlines[i], outlines[j])
-							if merge.size() == 0:
-								outlines.remove(i)
-								outlines.remove(j)
-							elif merge.size() == 1:
-								outlines[i] = merge[0]
-								outlines.remove(j)
-							elif merge.size() == 2:
-								outlines[i] = merge[0]
-								outlines[j] = merge[1]
-							elif merge.size() > 2:
-								outlines[i] = merge[0]
-								outlines[j] = merge[1]
-								for k in range(2, merge.size()):
-									outlines.append(merge[k])
-							j = 0
-					j += 1
-				i += 1
-		
-		
-		
-		var outer_points = PoolVector2Array([-size/2, Vector2(size.x, -size.y)/2, size/2, Vector2(-size.x, size.y)/2])
-		outlines.append(outer_points)
+		var outlines = get_outlines(skip)
 				
 		var nav_poly: NavigationPolygon = NavigationPolygon.new()
 		for outline in outlines:
@@ -109,6 +48,81 @@ func run_delete(_b):
 		remove_all_children()
 
 ##########################################################
+
+func get_outlines(skip: Array) -> Array:
+	var local_offset = offset
+	var outlines = []
+	
+	var bodies = get_node(map_path).get_children()
+	for body in bodies:
+		var colliders = body.get_children()
+		for collider in colliders:
+			var poly
+			if collider is CollisionPolygon2D:
+				poly = collider.polygon
+			elif collider is CollisionShape2D:
+				var shape = collider.shape
+				if shape is RectangleShape2D:
+					var ext = shape.extents
+					poly = PoolVector2Array([Vector2(-ext.x, -ext.y), Vector2(ext.x, -ext.y), Vector2(ext.x, ext.y), Vector2(-ext.x, ext.y)])
+				elif shape is CircleShape2D:
+					var rad = shape.radius
+					var local_circle_res = max(3, int(circle_res * rad * circle_res_growth))
+					local_offset = (offset + rad)/cos(PI / local_circle_res) - rad #https://en.wikipedia.org/wiki/Apothem
+					poly = PoolVector2Array()
+					for i in range(0, local_circle_res):
+						poly.push_back((Vector2.UP*rad).rotated(i * (2 * PI)/local_circle_res))
+				else:
+					continue
+			else:
+				continue
+				
+			for i in range(0, poly.size()):
+				var temp = poly[i].rotated(collider.rotation) + collider.position
+				poly.set(i, temp.rotated(body.rotation) + body.position)
+				
+			var new_poly = Geometry.offset_polygon_2d(poly, local_offset, Geometry.JOIN_MITER)[0]
+			outlines.append(new_poly)
+	
+	if merge:
+		var i = 0
+		while i < outlines.size():
+			var j = 0
+			while j < outlines.size():
+				if j != i:
+					if Geometry.intersect_polygons_2d(outlines[i], outlines[j]).size() > 0:
+						var merge = Geometry.merge_polygons_2d(outlines[i], outlines[j])
+						if merge.size() == 0:
+							outlines.remove(i)
+							outlines.remove(j)
+						elif merge.size() == 1:
+							outlines[i] = merge[0]
+							outlines.remove(j)
+						elif merge.size() == 2:
+							outlines[i] = merge[0]
+							outlines[j] = merge[1]
+						elif merge.size() > 2:
+							outlines[i] = merge[0]
+							outlines[j] = merge[1]
+							for k in range(2, merge.size()):
+								outlines.append(merge[k])
+						j = 0
+				j += 1
+			i += 1
+	
+	if flip == 0:
+		var new_outlines = []
+		for s in skip:
+			new_outlines.append(outlines[int(s)])
+		outlines = new_outlines
+	elif flip == 1:
+		for s in skip:
+			outlines.remove(s)
+	
+	var outer_points = PoolVector2Array([-size/2, Vector2(size.x, -size.y)/2, size/2, Vector2(-size.x, size.y)/2])
+	outlines.append(outer_points)
+	
+	return outlines
 
 func add_instance(navpoly):
 	var inst = NavigationPolygonInstance.new()
