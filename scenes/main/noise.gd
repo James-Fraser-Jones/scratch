50d5aef4060d813bc32ac6dadf0d2b7b1d144fdd
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-03-08 12:52:16 +0000

    first attempt at layering noise, layers kind of clash and block connectivity

--- scenes/main/noise.gd
+++ scenes/main/noise.gd
@@ -10,11 +10,12 @@ export var noise: OpenSimplexNoise
 export var noise_offset: Vector2 = Vector2.ZERO
 export var noise_size: Vector2 = Vector2.ONE * 512
 
-export var range_correction: bool = true
-
-export var add_threshold: bool = false
-export var threshold: float = 0
-export var threshold_color: Color = Color.red
+export var threshold_num: int = 1
+export var thresholds: Array = [
+	{"period_scale": 1.0, "threshold": 0.0, "index": -1, "color": Color.red}, 
+	{"period_scale": 0.5, "threshold": 0.0, "index": -2, "color": Color.green}, 
+	{"period_scale": 0.25, "threshold": 0.0, "index": -3, "color": Color.blue}
+]
 
 export var generate: bool setget run_generate
 export var delete: bool setget run_delete
@@ -23,7 +24,12 @@ func run_generate(_b):
 	if Engine.is_editor_hint():
 		scale = size / Vector2(pixel_width, pixel_height)
 		
-		var noise_grid = get_noise_grid()
+		var noise_grid = get_noise_grid(0)
+		var temp_seed = noise.seed
+		for i in range(1, threshold_num):
+			noise.seed += 1
+			noise_grid = merge_grids(noise_grid, get_noise_grid(i))
+		noise.seed = temp_seed
 		
 		var img = Image.new()
 		img.create(pixel_width, pixel_height, false, Image.FORMAT_RGB8)
@@ -31,8 +37,11 @@ func run_generate(_b):
 		for j in pixel_height:
 			for i in pixel_width:
 				var sample = noise_grid[j][i]
-				if sample == -2:
-					img.set_pixel(i, j, threshold_color)
+				if sample <= -1:
+					for entry in thresholds:
+						if sample == entry.index:
+							img.set_pixel(i, j, entry.color)
+							break
 				else:
 					img.set_pixel(i, j, Color.from_hsv(0, 0, (sample + 1)/2))
 		img.unlock()
@@ -47,7 +56,24 @@ func run_delete(_b):
 	if Engine.is_editor_hint():
 		texture = null
 
-func get_noise_grid() -> Array:
+func merge_grids(lower, upper) -> Array:
+	var noise_grid = []
+	for j in pixel_height:
+		var noise_row = []
+		for i in pixel_width:
+			var u = upper[j][i]
+			var l = lower[j][i]
+			if l <= -1:
+				noise_row.append(l)
+			else:
+				noise_row.append(u)
+		noise_grid.append(noise_row)
+	return noise_grid
+
+func get_noise_grid(thresh_index: int) -> Array:
+	var temp_period = noise.period
+	noise.period = noise.period * thresholds[thresh_index].period_scale
+	
 	var min_sample = 0
 	var max_sample = 0
 	
@@ -57,25 +83,23 @@ func get_noise_grid() -> Array:
 		for i in pixel_width:
 			var coord = Vector2(i, j) * noise_size / Vector2(pixel_width-1, pixel_height-1) + noise_offset
 			var sample = noise.get_noise_2dv(coord)
-			if range_correction:
-				min_sample = min(min_sample, sample)
-				max_sample = max(max_sample, sample)
+			min_sample = min(min_sample, sample)
+			max_sample = max(max_sample, sample)
 			noise_row.append(sample)
 		noise_grid.append(noise_row)
 		
-	if range_correction:
-		for j in pixel_height:
-			for i in pixel_width:
-				var current = noise_grid[j][i]
-				noise_grid[j][i] = lerp_solver(min_sample, max_sample, -1, 1, current)
+	for j in pixel_height:
+		for i in pixel_width:
+			var current = noise_grid[j][i]
+			noise_grid[j][i] = lerp_solver(min_sample, max_sample, -1, 1, current)
 	
-	if add_threshold:
-		for j in pixel_height:
-			for i in pixel_width:
-				var current = noise_grid[j][i]
-				if current <= threshold:
-					noise_grid[j][i] = -2
+	for j in pixel_height:
+		for i in pixel_width:
+			var current = noise_grid[j][i]
+			if current <= thresholds[thresh_index].threshold:
+				noise_grid[j][i] = thresholds[thresh_index].index
 	
+	noise.period = temp_period
 	return noise_grid
 
 #for (a <= x <= b) linearly-mapped to (c <= y <= d), find y
