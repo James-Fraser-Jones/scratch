1e5cbb739cc7e67453c3a558c4e418eeaa875866
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-03-08 11:42:04 +0000

    noise now has fixed physical size and fixed noise size independent of pixel resolution

--- scenes/main/noise.gd
+++ scenes/main/noise.gd
@@ -1,11 +1,14 @@
 tool
 extends Sprite
 
-export var width: int = 512
-export var height: int = 512
+export var size: Vector2 = Vector2.ONE * 512
+
+export var pixel_width: int = 512
+export var pixel_height: int = 512
 
 export var noise: OpenSimplexNoise
 export var noise_offset: Vector2 = Vector2.ZERO
+export var noise_size: Vector2 = Vector2.ONE * 512
 
 export var range_correction: bool = true
 
@@ -18,13 +21,15 @@ export var delete: bool setget run_delete
 
 func run_generate(_b):
 	if Engine.is_editor_hint():
+		scale = size / Vector2(pixel_width, pixel_height)
+		
 		var noise_grid = get_noise_grid()
 		
 		var img = Image.new()
-		img.create(width, height, false, Image.FORMAT_RGB8)
+		img.create(pixel_width, pixel_height, false, Image.FORMAT_RGB8)
 		img.lock()
-		for j in height:
-			for i in width:
+		for j in pixel_height:
+			for i in pixel_width:
 				var sample = noise_grid[j][i]
 				if sample == -2:
 					img.set_pixel(i, j, threshold_color)
@@ -38,15 +43,20 @@ func run_generate(_b):
 		
 		texture = tex
 
+func run_delete(_b):
+	if Engine.is_editor_hint():
+		texture = null
+
 func get_noise_grid() -> Array:
 	var min_sample = 0
 	var max_sample = 0
 	
 	var noise_grid = []
-	for j in height:
+	for j in pixel_height:
 		var noise_row = []
-		for i in width:
-			var sample = noise.get_noise_2d(i + noise_offset.x, j + noise_offset.y)
+		for i in pixel_width:
+			var coord = Vector2(i, j) * noise_size / Vector2(pixel_width-1, pixel_height-1) + noise_offset
+			var sample = noise.get_noise_2dv(coord)
 			if range_correction:
 				min_sample = min(min_sample, sample)
 				max_sample = max(max_sample, sample)
@@ -54,23 +64,19 @@ func get_noise_grid() -> Array:
 		noise_grid.append(noise_row)
 		
 	if range_correction:
-		for j in height:
-			for i in width:
+		for j in pixel_height:
+			for i in pixel_width:
 				var current = noise_grid[j][i]
 				noise_grid[j][i] = lerp_solver(min_sample, max_sample, -1, 1, current)
 	
 	if add_threshold:
-		for j in height:
-			for i in width:
+		for j in pixel_height:
+			for i in pixel_width:
 				var current = noise_grid[j][i]
 				if current <= threshold:
 					noise_grid[j][i] = -2
 	
 	return noise_grid
-
-func run_delete(_b):
-	if Engine.is_editor_hint():
-		texture = null
 
 #for (a <= x <= b) linearly-mapped to (c <= y <= d), find y
 func lerp_solver(a, b, c, d, x):
