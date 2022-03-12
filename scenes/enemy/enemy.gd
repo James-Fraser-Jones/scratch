e3d4bbf0dfb8912d3dca952a9b2cb5677b6a75de
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-03-12 19:52:20 +0000

    fixed bug in enemy collision avoidance causing them to get stuck to walls

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,8 +1,9 @@
 extends "res://scenes/character/character.gd"
 
 export var nav_freq : float = 2
-export var col_max : float = 75
-export var col_radius : float = 150
+export var col_max : float = 50
+export var col_radius : float = 150#
+export var cull_zone : float = 0.2
 
 onready var nav : Navigation2D = $"/root/main/nav"
 
@@ -16,25 +17,35 @@ func nav_timer(delta):
 			nav_cur = -1
 
 func avoid():
-	var avoid_vec = Vector2.ZERO
 	var space_state = get_world_2d().direct_space_state
+	var u = test_dir(space_state, Vector2( 0, -1))
+	var d = test_dir(space_state, Vector2( 0,  1))
+	var l = test_dir(space_state, Vector2(-1,  0))
+	var r = test_dir(space_state, Vector2( 1,  0))
+	var ul = test_dir(space_state, Vector2(-1, -1))
+	var ur = test_dir(space_state, Vector2( 1, -1))
+	var dl = test_dir(space_state, Vector2(-1,  1))
+	var dr = test_dir(space_state, Vector2( 1,  1))
 	
-	avoid_vec += test_dir(space_state, Vector2.UP)
-	avoid_vec += test_dir(space_state, Vector2.DOWN)
-	avoid_vec += test_dir(space_state, Vector2.LEFT)
-	avoid_vec += test_dir(space_state, Vector2.RIGHT)
+	var avoid_vec = Vector2.ZERO
+	avoid_vec += u
+	avoid_vec += d
+	avoid_vec += l
+	avoid_vec += r
+	avoid_vec += ul
+	avoid_vec += ur
+	avoid_vec += dl
+	avoid_vec += dr
 	
-	avoid_vec += test_dir(space_state, Vector2.ONE)
-	avoid_vec += test_dir(space_state, -Vector2.ONE)
-	avoid_vec += test_dir(space_state, Vector2(1, -1))
-	avoid_vec += test_dir(space_state, -Vector2(1, -1))
-		
 	if avoid_vec.length() > 1:
 		avoid_vec = avoid_vec.normalized()
+	elif avoid_vec.length() < cull_zone:
+		avoid_vec = Vector2.ZERO
+		
 	return avoid_vec
 
 func test_dir(space_state, dir : Vector2) -> Vector2:
-	var result = space_state.intersect_ray(global_position, global_position + dir * col_radius, [self])
+	var result = space_state.intersect_ray(global_position, global_position + dir.normalized() * col_radius, [self])
 	if result:
 		var dist = (result.position - global_position).length()
 		return -dir * scale_response(dist)
@@ -44,23 +55,18 @@ func scale_response(dist):
 	var response = min(1, dist / (col_max-col_radius) + (col_radius/(col_radius-col_max)))
 	return response
 
-func _process(delta):
-	if get_tree().has_group("players"):
-		var player = get_tree().get_nodes_in_group("players")[0]
-		var look_vec = player.position - position
-		set_rot(look_vec.angle())
-
 func _physics_process(delta):
 	nav_timer(delta)
 	
 	if get_tree().has_group("players"):
 		var player = get_tree().get_nodes_in_group("players")[0]
 		
+		var look_vec = player.position - position
+		set_rot(look_vec.angle())
 		var space_state = get_world_2d().direct_space_state
 		var result = space_state.intersect_ray(position, player.position, get_tree().get_nodes_in_group("enemies"))
 		if result and result.collider == player:
-			var shoot_vec = player.position - position
-			$gun.shoot(shoot_vec)
+			$gun.shoot(look_vec)
 		
 		if nav_cur == -1:
 			nav_path = nav.get_simple_path(position, player.position)
@@ -76,4 +82,4 @@ func _physics_process(delta):
 			var avoid_vec = avoid()
 			#print(rad2deg(avoid_vec.angle()))
 			var move_vec = avoid_vec + goal_vec * (1 - avoid_vec.length())
-			move(goal_vec * move_quota)
+			move(move_vec * move_quota)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,15 +1,12 @@
 extends "res://scenes/character/character.gd"
 
 func _physics_process(delta):
-	if Input.is_action_pressed("shoot"):
-		var shoot_vec = get_global_mouse_position() - position
-		$gun.shoot(shoot_vec)
-	var move_vec = lrud() * speed * delta
-	move(move_vec)
-
-func _process(delta):
 	var look_vec = get_global_mouse_position() - position
 	set_rot(look_vec.angle())
+	if Input.is_action_pressed("shoot"):
+		$gun.shoot(look_vec)
+	var move_vec = lrud() * speed * delta
+	move(move_vec)
 
 func lrud() -> Vector2:
 	var input : Vector2 = Vector2.ZERO
