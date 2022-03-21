6a0d55497d0e4090552a9305dee0bc095e7dcd1d
smudgecat123 <jf14218@my.bristol.ac.uk>
2022-03-21 14:26:46 +0000

    added healthbar hiding after not taking damage

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -3,8 +3,17 @@ extends KinematicBody2D
 export var max_health : int = 100
 export var speed : float = 500
 export var god : bool = false
+export var health_visible_time : float = 5
 
 var health : int = max_health
+var cur_health_visible_time : float = -1
+
+func _process(delta):
+	if cur_health_visible_time >= 0:
+		cur_health_visible_time += delta
+		if cur_health_visible_time >= health_visible_time:
+			cur_health_visible_time = -1
+			$health_bar.visible = false
 
 func move(move_vec):
 	var collision = move_and_collide(move_vec)
@@ -15,6 +24,8 @@ func hurt(damage):
 	if !god:
 		health -= damage
 		$health_bar.value = health
+		cur_health_visible_time = 0
+		$health_bar.visible = true
 		if health <= 0:
 			queue_free()
 
